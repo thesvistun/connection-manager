d5c4c91ede13073ab60bc9ce31b17b865b620cbd
svistun <themazay@gmail.com>
2017-10-29 23:23:08 +0300

    Add Configuration.

---
+++ src/main/java/name/svistun/http/Configuration/Config.java
@@ -0,0 +1,55 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Svistunov Aleksey
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package name.svistun.http.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+import name.svistun.http.ProxySource;
+import org.apache.commons.configuration2.XMLConfiguration;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.log4j.Logger;
+
+public class Config {
+    private XMLConfiguration config;
+    private static final Logger log = Logger.getLogger(Config.class.getSimpleName());
+
+    public Config(String file) throws ConfigurationException {
+        config = new Configurations().xml(file);
+    }
+
+    public List<ProxySource> getProxyServers() {
+        List<ProxySource> proxySources = new ArrayList<>();
+        List<Object> proxies = config.getList(ConfigProps.URL_KEY);
+        for (int i = 0; i < proxies.size(); i++) {
+            String url = config.getString(String.format("%s(%s).%s", ConfigProps.PROXY_SOURCE_KEY, i, ConfigProps.URL));
+            String offset = config.getString(String.format("%s(%s).%s", ConfigProps.PROXY_SOURCE_KEY, i, ConfigProps.OFFSET));
+            ProxySource proxySource = new ProxySource(url, offset);
+            log.debug(proxySource);
+            proxySources.add(proxySource);
+        }
+        return proxySources;
+    }
+}
---
+++ src/main/java/name/svistun/http/Configuration/ConfigProps.java
@@ -0,0 +1,36 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Svistunov Aleksey
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package name.svistun.http.Configuration;
+
+public interface ConfigProps {
+    String ROOT = "connection-manager";
+    String PROXIES = "proxies";
+    String PROXIES_KEY = String.format("%s.%s",ROOT, PROXIES);
+    String PROXY_SOURCE = "proxy-source";
+    String PROXY_SOURCE_KEY = String.format("%s.%s",PROXIES_KEY, PROXY_SOURCE);
+    String URL = "url";
+    String URL_KEY = String.format("%s.%s", PROXY_SOURCE_KEY, URL);
+    String OFFSET = "offset";
+}
